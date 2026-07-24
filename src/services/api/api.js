d24e6676505cae5c0a45fa1d5a44fbe36ba4960d
Karen Ruiz <katiiruiz797@gmail.com>
2026-07-24 20:32:38 -0300

    fix: agregar interceptor Bearer Token de Keycloak en cliente api.js

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import keycloak from "@/config/keycloak";
 
 const api = axios.create({
     baseURL: "http://localhost:8080",
 });
+
+api.interceptors.request.use(
+    (config) => {
+        if (keycloak && keycloak.token) {
+            config.headers.Authorization = `Bearer ${keycloak.token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
